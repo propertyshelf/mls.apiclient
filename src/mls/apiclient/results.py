790f50e4db890693446fd581ef99ffe202fcbd14
Thomas Massmann <thomas@propertyshelf.com>
2014-08-20 12:04:30 +0200

    Added base entity result class.

---
+++ src/mls/apiclient/results.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""MLS rest client result classes."""
+
+
+class ResultBase(object):
+    """Base class for results."""
+
+    def __init__(self, data):
+        if not isinstance(data, dict):
+            raise ValueError(
+                'Data must be dictionary with content of the result'
+            )
+        self._data = data
+
+    def __getattr__(self, name):
+        """Returns an data attribute or raises AttributeError."""
+        try:
+            return self._data[name]
+        except KeyError:
+            return object.__getattribute__(self, name)
+
+    def get_attributes(self):
+        """Returns a list of all attributes of a Result object."""
+        return self._data.keys()
---
+++ src/mls/apiclient/tests/test_results.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""Test the result classes."""
+
+# python imports
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+# local imports
+from mls.apiclient.results import ResultBase
+
+
+class ResultTestCase(unittest.TestCase):
+    """Test result class."""
+
+    def _callFUT(self, data):
+        return ResultBase(data)
+
+    def test_wrong_data(self):
+        """Validate the ValueError if data is not a dictionary."""
+        self.assertRaises(ValueError, self._callFUT, None)
+        self.assertRaises(ValueError, self._callFUT, 1)
+        self.assertRaises(ValueError, self._callFUT, 1.1)
+        self.assertRaises(ValueError, self._callFUT, True)
+        self.assertRaises(ValueError, self._callFUT, 'Foo')
+        self.assertRaises(ValueError, self._callFUT, u'Foo')
+
+    def test_correct_data(self):
+        """Validate the correct initialization with a dictionary."""
+        data = {'foo': 'bar'}
+        foo = self._callFUT(data)
+        self.assertIsInstance(foo, ResultBase)
+        self.assertEqual(data, foo._data)
+
+    def test_attributes(self):
+        data = {'foo': 'bar'}
+        foo = self._callFUT(data)
+        self.assertEqual(foo.foo, 'bar')
+
+    def test_missing_attributes(self):
+        data = {'foo': 'bar'}
+        foo = self._callFUT(data)
+        self.assertRaises(AttributeError, getattr, foo, 'bar')
+
+    def test_get_attributes(self):
+        """Validate the output of the get_attributes method."""
+        data = {
+            'foo': 'bar',
+            'baz': 'barbar',
+        }
+        foo = self._callFUT(data)
+        result = foo.get_attributes()
+        self.assertIn('foo', result)
+        self.assertIn('baz', result)
+        self.assertEqual(len(result), 2)
