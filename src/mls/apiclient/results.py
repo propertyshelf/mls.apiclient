3accfc62b8a647eef6dfde660727661abcded770
Thomas Massmann <thomas@propertyshelf.com>
2014-08-20 12:08:49 +0200

    Added more result entity classes.

--- src/mls/apiclient/results.py
+++ src/mls/apiclient/results.py
@@ -22,3 +22,27 @@ class Result(object):
     def get_attributes(self):
         """Returns a list of all attributes of a Result object."""
         return self._data.keys()
+
+
+class Agency(Result):
+    """'Agency' entity result class."""
+
+
+class Agent(Result):
+    """'Agent' entity result class."""
+
+
+class Development(Result):
+    """'Development Project' entity result class."""
+
+
+class DevelopmentPhase(Result):
+    """'Development Phase' entity result class."""
+
+
+class Listing(Result):
+    """'Listing' entity result class."""
+
+
+class PropertyGroup(Result):
+    """'Property Group' entity result class."""
