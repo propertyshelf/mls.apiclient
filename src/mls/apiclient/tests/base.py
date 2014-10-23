f592dc37aa017fa1dbf7d9b4bb4ae38971f6dbe5
Thomas Massmann <thomas@propertyshelf.com>
2014-10-23 16:40:26 +0200

    Added integration test setup.

--- src/mls/apiclient/tests/base.py
+++ src/mls/apiclient/tests/base.py
@@ -2,10 +2,18 @@
 """Test case base class."""
 
 # python imports
+import httpretty
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest
+
+# local imports
+from mls.apiclient import (
+    REST_API_URL,
+    REST_API_VERSION,
+)
+from mls.apiclient.tests import utils
 
 
 class BaseTestCase(unittest.TestCase):
@@ -22,3 +30,18 @@ class BaseTestCase(unittest.TestCase):
     def URL(self):
         """Return the MLS API endpoint url for the tests."""
         return self.BASE_URL + self.PATH
+
+    def setup_integration_test(self):
+        """Setup all URL mocks to run a full integration test."""
+        API_BASE = '/'.join((self.BASE_URL, REST_API_URL, REST_API_VERSION))
+
+        def _register(endpoint, content=None, fixture=None):
+            if fixture:
+                content = utils.load_fixture(fixture)
+            httpretty.register_uri(
+                httpretty.GET,
+                utils.get_url(API_BASE, endpoint),
+                body=content,
+            )
+
+        _register('developments', fixture='development_list_en.json')
