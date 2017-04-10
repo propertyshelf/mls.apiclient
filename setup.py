49329841eca20cfe18700bafe2d59fb373621231
Thomas Massmann <thomas@propertyshelf.com>
2017-04-10 11:19:45 +0200

    Move responses requirement to test section.

--- setup.py
+++ setup.py
@@ -38,13 +38,13 @@ setup(
     zip_safe=False,
     extras_require=dict(
         test=[
+            'responses',
             'unittest2',
         ],
     ),
     install_requires=[
         'setuptools',
         'requests',
-        'responses',
     ],
     entry_points="""""",
 )
